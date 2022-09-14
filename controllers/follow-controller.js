5407dafce8596a62eac53b7816d65965d6fbd579
K N Dikshith <dikshithomesh@gmail.com>
2022-09-14 16:53:52 +0530

    status code

--- controllers/follow-controller.js
+++ controllers/follow-controller.js
@@ -2,7 +2,9 @@ const followSchema = require('../schema/followSchema')
 const notificationSchema = require('../schema/notificationSchema')
 const userSchema = require('../schema/userSchema')
 async function follower(req,res,next){
-    
+    if(!req.body.follow){
+        return res.status(400).send("Invalid reqeust body")
+    }
     const user = userSchema.findOne({user:req.body.follow})
     if(user== null){
         return res.send("user not found")
@@ -11,11 +13,11 @@ async function follower(req,res,next){
     console.log(value)
     if(value != null){
         await followSchema.deleteOne({followedBy:req.body.user.name,following:req.body.follow})
-        return res.send("unfollowed successfully")
+        return res.status(200).send("unfollowed successfully")
     }
     const follow= new followSchema({followedBy:req.body.user.name,following:req.body.follow})
     await follow.save()
-    res.send("following successfull")
+    res.status(201).send("following successfull")
 }
 
 //section of code to push onto notifier when new articles is published by user 
@@ -36,10 +38,10 @@ async function notified(req,res,next){
     console.log("let's see if this works")
     const notifications =await notificationSchema.find({updateFor:req.body.user.name})
     if(notifications == []){
-        return res.send("not notifications here")
+        return res.status(200).send("not notifications here")
     } 
     
-    res.send(notifications)
+    res.status(200).send(notifications)
     await notificationSchema.deleteMany({updateFor:req.body.user.name})
 }
 
