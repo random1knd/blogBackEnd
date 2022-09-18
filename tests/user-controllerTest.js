0ce69a83e1fda59dfa76b1436a409966a8244f34
K N Dikshith <dikshithomesh@gmail.com>
2022-09-18 16:16:18 +0530

    registration and user login tests completed and working

--- tests/user-controllerTest.js
+++ tests/user-controllerTest.js
@@ -45,92 +45,105 @@ describe("user controller check",()=>{
             it("password missing expected password is requried",(done)=>{
                 const payload = {user:"dikshith",description:"somedescription",email:"dikshith@123"};
                 request.post(`${baseurl}/register`,{json:payload},(_,response)=>{
-                    console.log(`let's see if the response changes${response.body.message}`)
+                //console.log(response.body)
+                expect(response.body.message).to.equal("password is required and with the right length min 7 characters");
+                done();
 
-                })
-            })
+
+                });
+            });
+
+         
+            
+            it("password missing expected password is requried",(done)=>{
+                const payload = {user:"dikshith",password:"password123",description:"",email:"dikshith@123"};
+                request.post(`${baseurl}/register`,{json:payload},(_,response)=>{
+                //console.log(response.body)
+                expect(response.body.message).to.equal("description is required");
+                done();
+
+                
+                });
+            });
+
+            
+            it("password missing expected password is requried",(done)=>{
+                const payload = {user:"dikshith",password:"password123",description:"somedescription",email:""};
+                request.post(`${baseurl}/register`,{json:payload},(_,response)=>{
+                //console.log(response.body)
+                expect(response.body.message).to.equal("email is required");
+                done();
+
+                
+                });
+            });
+
+        });
+    });
+});
+
+
+
+describe("user login test",()=>{
+    describe("username or password missing",()=>{
+        describe("username not present or password is wrong",()=>{
+            
+            it("username missing",(done)=>{
+                const payload = {user:"",password:"password123"};
+                request.post(`${baseurl}/login`,{json:payload},(_,response)=>{
+                //console.log(response.body)
+                expect(response.statusCode).to.equal(400);
+                
+                expect(response.body.message).to.equal("both username and password are required");
+                done();
+
+                
+                });
+            });
+
+
+            it("password missing",(done)=>{
+                const payload = {user:"dikshith",password:""};
+                request.post(`${baseurl}/login`,{json:payload},(_,response)=>{
+                //console.log(response.body)
+                expect(response.statusCode).to.equal(400);
+                expect(response.body.message).to.equal("both username and password are required");
+                done();
+
+                
+                });
+            });
+
+
+            it("password missing",(done)=>{
+                const payload = {user:"dikshith",password:"asdfsadf"};
+                request.post(`${baseurl}/login`,{json:payload},(_,response)=>{
+                //console.log(response.body)
+                expect(response.statusCode).to.equal(403);
+                expect(response.body.message).to.equal("password does not match");
+                done();
+
+                
+                });
+            });
+
+
+            it("password missing",(done)=>{
+                const payload = {user:"dikshith",password:"dikshith123"};
+                request.post(`${baseurl}/login`,{json:payload},(_,response)=>{
+                //console.log(response.body)
+                expect(response.statusCode).to.equal(200);
+                expect(response.body.message).to.equal("logged in");
+                done();
+
+                
+                });
+            });
+
+
 
          
 
         });
     });
 });
-
-describe("user login tests",()=>{
-    describe("user name missing and password missing",()=>{
-        describe("user doesn't exists or password is wrong ",()=>{
-            it("user name is missing",(done)=>{
-                const payload = {password:"password"};
-                request.post(`${baseurl}/login`,{json:payload},(_,response)=>{
-                    console.log(`from user name is missing login ${response.body}`)
-                    // console.log(response.body)
-                    expect(response.statusCode).to.equal(400);
-
-                    done();
-                });
-            });
-
-            it("password is missing",(done)=>{
-                const payload = {user:"dikshith",password:""};
-                request.post(`${baseurl}/login`,{json:payload},(_,response)=>{
-                    
-                    expect(response.statusCode).to.equal(400);
-                    done();
-                });
-            });
-
-
-            it("missing user name error",(done)=>{
-                const payload = {user:"dikshith",password:"password"};
-                request.post(`${baseurl}/login`,{json:payload},(_,response)=>{
-                    const value = JSON.parse(response.body);
-                    //console.log(value)
-                    expect(value.message).to.equal("both username and password are required");
-                    done();
-                });
-            });
-
-
-            it("missing password error",(done)=>{
-                const payload = {user:"dikshith",password:""};
-                request.post(`${baseurl}/login`,{json:payload},(_,response)=>{
-                    const value = JSON.parse(response.body);
-                    //console.log(value)
-                    expect(value.message).to.equal("both username and password are required");
-                    done();
-                });
-            });
-
-            
-
-
-
-
-
-        });
-    });
-});
-// desctibe("different one",()=>{
-//     it("value are not different",(done)=>{
-//         const values = {user:"dikshith",password:"",email:"dikshith@123",description:"this is about me"}
-//         request.post(`${baseurl}/register`,payload,(_,response)=>{
-//             expect(response.body.message).to.equal("password is required");
-//             done();
-//         });
-//     });
-// });
-
-describe("password is wrong",()=>{
-    it("password is wrong ",(done)=>{
-        const payload = {user:"dikshith",password:"password"}
-        request.post(`${baseurl}/login`,{json:payload},(_,response)=>{
-            console.log(`password is wrong ${response.body}`);
-            expect(response.body.message).to.equal("password does not match");
-            
-            done();
-        })
-    })
- 
-
-
-})
